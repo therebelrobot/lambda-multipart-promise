72ed33ef947a96b19c336b859a6077adb87694da
Oz Haven (Trent Oswald) <ozzyhaven@therebelrobot.com>
2018-05-09 16:34:01 -0700

    fixed parser

--- multipart.js
+++ multipart.js
@@ -1,44 +1,40 @@
-var Multipart = require('lambda-multipart');
+// https://stackoverflow.com/a/49169607
+const Busboy = require('busboy');
+const YError = require('yerror');
+const getRawBody = require('raw-body');
+let log = () => null;
 
-module.exports = (event) => new Promise((resolve, reject) => {
-  try {
-    const results = { fields: {}, fieldsList: [], files: [] };
-    resolve(results);
-    // const parser = new Multipart(event);
-    //
-    // parser.on('field', (key, value) => {
-    //   try {
-    //     results.fields[key] = value;
-    //     results.fieldsList.push({ key, value });
-    //   } catch (e) {
-    //     reject(e)
-    //   }
-    // });
-    //
-    // parser.on('file', (file) => {
-    //   try {
-    //     results.files.push(file);
-    //   } catch (e) {
-    //     reject(e)
-    //   }
-    // });
-    //
-    // parser.on('finish', () => {
-    //   try {
-    //     resolve(results);
-    //   } catch (e) {
-    //     reject(e)
-    //   }
-    // });
-    //
-    // parser.on('error', (error) => {
-    //   try {
-    //     reject(error);
-    //   } catch (e) {
-    //     reject(e)
-    //   }
-    // });
-  } catch (e) {
-    reject(e)
-  }
-})
+module.exports = (content, headers, opts) => new Promise((resolve, reject) => {
+  if (opts.verbose) { log = console.log };
+  headers['content-type'] = headers['Content-Type'];
+  log('Initializing Parse', content, headers);
+  const filePromises = [];
+  const data = {};
+  const parser = new Busboy({ headers });
+
+  parser.on('field', (name, value) => {
+    log('parsed field', name, value)
+    data[name] = value;
+  });
+  parser.on('file', (name, file, filename, encoding, mimetype) => {
+    log('parsed file', name, filename, encoding, mimetype)
+    data[name] = {
+      filename,
+      encoding,
+      mimetype,
+    };
+    filePromises.push(
+      getRawBody(file).then(rawFile => (data[name].content = rawFile))
+    );
+  });
+  parser.on('error', err => {
+    log('error during parse')
+    reject(YError.wrap(err))
+  });
+  parser.on('finish', () => {
+    log('finished parse')
+    return resolve(Promise.all(filePromises).then(() => data))
+  });
+  parser.write(content);
+  parser.end();
+});
