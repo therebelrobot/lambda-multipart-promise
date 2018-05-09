79d6a8d36bb24159f9d469ebe1d6eed83eded9c0
Oz Haven (Trent Oswald) <ozzyhaven@therebelrobot.com>
2018-05-09 14:54:17 -0700

    wrapped in try/catch

--- multipart.js
+++ multipart.js
@@ -1,23 +1,27 @@
 var Multipart = require('lambda-multipart');
 
 module.exports = (event) => new Promise((resolve, reject) => {
-  const results = { fields: {}, fieldsList: [], files: [] };
-  const parser = new Multipart(event);
+  try {
+    const results = { fields: {}, fieldsList: [], files: [] };
+    const parser = new Multipart(event);
 
-  parser.on('field',function(key, value){
-    results.fields[key] = value;
-    results.fieldsList.push({ key, value });
-  });
+    parser.on('field',function(key, value){
+      results.fields[key] = value;
+      results.fieldsList.push({ key, value });
+    });
 
-  parser.on('file',function(file){
-    results.files.push(file);
-  });
+    parser.on('file',function(file){
+      results.files.push(file);
+    });
 
-  parser.on('finish',function(){
-    resolve(results);
-  });
+    parser.on('finish',function(){
+      resolve(results);
+    });
 
-  parser.on('error',function(error){
-    reject(error);
-  });
+    parser.on('error',function(error){
+      reject(error);
+    });
+  } catch (e) {
+    reject(e)
+  }
 })
