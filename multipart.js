6e13d1e52ac9824062417d23c514c15c304ec1e7
Oz Haven (Trent Oswald) <ozzyhaven@therebelrobot.com>
2018-05-09 15:04:46 -0700

    testing

--- multipart.js
+++ multipart.js
@@ -3,40 +3,41 @@ var Multipart = require('lambda-multipart');
 module.exports = (event) => new Promise((resolve, reject) => {
   try {
     const results = { fields: {}, fieldsList: [], files: [] };
-    const parser = new Multipart(event);
-
-    parser.on('field', (key, value) => {
-      try {
-        results.fields[key] = value;
-        results.fieldsList.push({ key, value });
-      } catch (e) {
-        reject(e)
-      }
-    });
-
-    parser.on('file', (file) => {
-      try {
-        results.files.push(file);
-      } catch (e) {
-        reject(e)
-      }
-    });
-
-    parser.on('finish', () => {
-      try {
-        resolve(results);
-      } catch (e) {
-        reject(e)
-      }
-    });
-
-    parser.on('error', (error) => {
-      try {
-        reject(error);
-      } catch (e) {
-        reject(e)
-      }
-    });
+    resolve(results);
+    // const parser = new Multipart(event);
+    //
+    // parser.on('field', (key, value) => {
+    //   try {
+    //     results.fields[key] = value;
+    //     results.fieldsList.push({ key, value });
+    //   } catch (e) {
+    //     reject(e)
+    //   }
+    // });
+    //
+    // parser.on('file', (file) => {
+    //   try {
+    //     results.files.push(file);
+    //   } catch (e) {
+    //     reject(e)
+    //   }
+    // });
+    //
+    // parser.on('finish', () => {
+    //   try {
+    //     resolve(results);
+    //   } catch (e) {
+    //     reject(e)
+    //   }
+    // });
+    //
+    // parser.on('error', (error) => {
+    //   try {
+    //     reject(error);
+    //   } catch (e) {
+    //     reject(e)
+    //   }
+    // });
   } catch (e) {
     reject(e)
   }
